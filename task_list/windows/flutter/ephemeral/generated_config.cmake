a348dccb4455eff54ee1e1064636379f096ecb1f
Rafael-Maffei <rluizmaffei@gmail.com>
2022-11-14 00:08:14 -0300

    Tentativa de reconhecer a voz do usuário.

---
+++ task_list/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\aluno\\Documents\\PDM_RM\\task_list" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\aluno\\Documents\\PDM_RM\\task_list"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\aluno\\Documents\\PDM_RM\\task_list\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\aluno\\Documents\\PDM_RM\\task_list"
+  "FLUTTER_TARGET=C:\\Users\\aluno\\Documents\\PDM_RM\\task_list\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\aluno\\Documents\\PDM_RM\\task_list\\.dart_tool\\package_config.json"
+)
